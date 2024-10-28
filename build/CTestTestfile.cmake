12b2907a472bcf4e756a53290f461c9b6d0b8eee
fx <fx@test.local>
2024-10-28 13:24:09 +0300

    lab01-5-google

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -6,3 +6,5 @@
 # testing this directory and lists subdirectories to be tested as well.
 add_test(test_version "test_version")
 set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "/home/fx/projects/otus-cpp/otus-cpp/CMakeLists.txt;85;add_test;/home/fx/projects/otus-cpp/otus-cpp/CMakeLists.txt;0;")
+add_test(test_version_google "test_version_google")
+set_tests_properties(test_version_google PROPERTIES  _BACKTRACE_TRIPLES "/home/fx/projects/otus-cpp/otus-cpp/CMakeLists.txt;86;add_test;/home/fx/projects/otus-cpp/otus-cpp/CMakeLists.txt;0;")
