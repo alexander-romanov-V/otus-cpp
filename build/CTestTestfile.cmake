72f580d9398451880da0654f616a24afbba13b55
fx <fx@test.local>
2024-10-29 00:35:43 +0300

    lab02-8-cmake-test

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -4,5 +4,5 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(ipfilter_cli "sh" "-c" "cat /home/fx/projects/otus-cpp/otus-cpp/build/ip_filter.tsv | ./ipfilter_cli | md5sum | grep -q '24e7a7b2270daee89c64d3ca5fb3da1a'")
+add_test(ipfilter_cli "sh" "-c" "cat /home/fx/projects/otus-cpp/otus-cpp/build/ip_filter.tsv | /home/fx/projects/otus-cpp/otus-cpp/build/ipfilter_cli | md5sum | grep -q '24e7a7b2270daee89c64d3ca5fb3da1a'")
 set_tests_properties(ipfilter_cli PROPERTIES  WORKING_DIRECTORY "/home/fx/projects/otus-cpp/otus-cpp/build" _BACKTRACE_TRIPLES "/home/fx/projects/otus-cpp/otus-cpp/CMakeLists.txt;56;add_test;/home/fx/projects/otus-cpp/otus-cpp/CMakeLists.txt;0;")
