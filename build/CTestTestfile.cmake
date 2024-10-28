74967b96f082c9a89d57ddf0f7ef7699a9fd1863
fx <fx@test.local>
2024-10-28 12:38:19 +0300

    lab01-2

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/fx/projects/otus-cpp/otus-cpp
+# Build directory: /home/fx/projects/otus-cpp/otus-cpp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_version "test_version")
+set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "/home/fx/projects/otus-cpp/otus-cpp/CMakeLists.txt;74;add_test;/home/fx/projects/otus-cpp/otus-cpp/CMakeLists.txt;0;")
