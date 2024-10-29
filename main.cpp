53f2d2f12a7ba607643e1a7665acb398fdfd4132
fx <fx@test.local>
2024-10-29 19:47:23 +0300

    lab-02-C-cmake-test

--- main.cpp
+++ main.cpp
@@ -3,86 +3,60 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
 #include <algorithm>
+#include <array>
 
-#define UNUSED(var) (void)var
+typedef std::array<short, 4> IP;
+typedef std::vector<IP> IPList;
 
-// ("",  '.') -> [""]
-// ("11", '.') -> ["11"]
-// ("..", '.') -> ["", "", ""]
-// ("11.", '.') -> ["11", ""]
-// (".11", '.') -> ["", "11"]
-// ("11.22", '.') -> ["11", "22"]
-std::vector<std::string> split(const std::string &str, char d)
-{
-    std::vector<std::string> r;
-
+// DONE extract IP from string     "I.I.I.I\t..." -> {I, I, I, I}
+IP getIP(std::string s) {
+    IP res{0, 0, 0, 0};
+    auto i = 0;
     std::string::size_type start = 0;
-    std::string::size_type stop = str.find_first_of(d);
-    while(stop != std::string::npos)
+    std::string::size_type stop = s.find_first_of(".");
+    while (stop != std::string::npos && i < 4)
     {
-        r.push_back(str.substr(start, stop - start));
-
+        res[i++] = std::stoi(s.substr(start, stop - start));
         start = stop + 1;
-        stop = str.find_first_of(d, start);
+        stop = s.find_first_of(".\t ", start);
     }
-
-    r.push_back(str.substr(start));
-
-    return r;
+    return res;
 }
 
-int main()
-{
-    // std::cout << "debug 1\n";
+// DONE print function
+auto print_pool = [](auto const& p) { 
+    for (auto &&i : p) { 
+        std::cout << i[0] << "." << i[1] << "." << i[2] << "." << i[3] << std::endl;
+    };
+};
 
-    try
-    {
-        std::vector<std::vector<std::string> > ip_pool;
+int main() {
+    try {
 
+        // DONE Read IP list
+        IPList ip_pool;
         for(std::string line; std::getline(std::cin, line);)
         {
-            std::vector<std::string> v = split(line, '\t');
-            ip_pool.push_back(split(v.at(0), '.'));
+            ip_pool.emplace_back(getIP(line));
         }
 
-        // DONE Convert into int sub arrays
-        std::vector<std::vector<int>> n_pool;
-        std::transform(ip_pool.begin(), ip_pool.end(), back_inserter(n_pool), [](auto const& l) { 
-                std::vector<int> v;
-                for (auto &&i : l) {
-                    v.push_back(std::stoi(i));
-                }
-                return v;
-            });
-
-        // DONE print function
-        auto print_pool = [](auto const& title, auto const& p) { 
-                UNUSED(title); // std::cout <<std::endl << title << std::endl;
-                for (auto &&i : p) { 
-                    std::cout << i[0] << "." << i[1] << "." << i[2] << "." << i[3] << std::endl;
-                };
-            };
         
-        // DONE reverse lexicographically sort
+        // DONE reverse lexicographically sort and output
         // 222.173.235.246
         // 222.130.177.64
-        // 222.82.198.61
         // ...
-        // 1.70.44.170
         // 1.29.168.152
         // 1.1.234.8
-            // std::vector<std::vector<int>> n_reverse = n_pool;
-            // std::sort(n_reverse.begin(), n_reverse.end(), [](auto &a, auto &b){
-        std::sort(n_pool.begin(), n_pool.end(), [](auto &a, auto &b){
+        std::sort(ip_pool.begin(), ip_pool.end(), [](auto &a, auto &b){
                 for (size_t i = 0; i < 4; i++) {
                     if (a[i] < b[i]) return false;
                     if (a[i] > b[i]) return true;
                 }
                 return true;
             });
-        print_pool("Reverse lexicographically sort", n_pool);
+        print_pool(ip_pool);
+
 
         // DONE filter by first byte and output
         // ip = filter(1)
@@ -91,11 +65,11 @@ int main()
         // 1.70.44.170
         // 1.29.168.152
         // 1.1.234.8
-        std::vector<std::vector<int>> n_pool_1;
-        std::copy_if(n_pool.begin(), n_pool.end(), std::back_inserter(n_pool_1), [n=1](auto const& ip){ 
+        IPList ip_pool_1;
+        std::copy_if(ip_pool.begin(), ip_pool.end(), std::back_inserter(ip_pool_1), [n=1](auto const& ip){ 
                 return ip[0] == n;
             ;});
-        print_pool("Filter by first byte and output, ip = filter(1)", n_pool_1);
+        print_pool(ip_pool_1);
 
 
         // DONE filter by first and second bytes and output
@@ -104,54 +78,25 @@ int main()
         // 46.70.147.26
         // 46.70.113.73
         // 46.70.29.76
-        std::vector<std::vector<int>> n_pool_1_2;
-        std::copy_if(n_pool.begin(), n_pool.end(), std::back_inserter(n_pool_1_2), [n1=46,n2=70](auto const& ip){ 
+        IPList ip_pool_1_2;
+        std::copy_if(ip_pool.begin(), ip_pool.end(), std::back_inserter(ip_pool_1_2), [n1=46,n2=70](auto const& ip){ 
                 return ip[0] == n1 && ip[1] == n2;
             ;});
-        print_pool("Filter by first and second bytes and output, ip = filter(46, 70)", n_pool_1_2);
+        print_pool(ip_pool_1_2);
 
 
         // DONE filter by any byte and output
         // ip = filter_any(46)
         // 186.204.34.46
         // 186.46.222.194
-        // 185.46.87.231
-        // 185.46.86.132
-        // 185.46.86.131
-        // 185.46.86.131
-        // 185.46.86.22
-        // 185.46.85.204
-        // 185.46.85.78
-        // 68.46.218.208
-        // 46.251.197.23
-        // 46.223.254.56
-        // 46.223.254.56
-        // 46.182.19.219
-        // 46.161.63.66
-        // 46.161.61.51
-        // 46.161.60.92
-        // 46.161.60.35
-        // 46.161.58.202
-        // 46.161.56.241
-        // 46.161.56.203
-        // 46.161.56.174
-        // 46.161.56.106
-        // 46.161.56.106
-        // 46.101.163.119
-        // 46.101.127.145
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-        // 46.55.46.98
-        // 46.49.43.85
+        // ...
         // 39.46.86.85
         // 5.189.203.46
-        std::vector<std::vector<int>> n_pool_any;
-        std::copy_if(n_pool.begin(), n_pool.end(), std::back_inserter(n_pool_any), [n=46](auto const& ip){ 
+        IPList ip_pool_any;
+        std::copy_if(ip_pool.begin(), ip_pool.end(), std::back_inserter(ip_pool_any), [n=46](auto const& ip){ 
                 return ip[0] == n || ip[1] == n || ip[2] == n || ip[3] == n;
             ;});
-        print_pool("Filter by any byte and output, ip = filter_any(46)", n_pool_any);
+        print_pool(ip_pool_any);
         
     }
     catch(const std::exception &e)
